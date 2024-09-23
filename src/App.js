66365d64b94e3e206c92efbd7992c04ea75740a7
maminebnr <m.benrhouma@intel-think.com>
2024-09-23 11:46:32 +0100

    change

--- src/App.js
+++ src/App.js
@@ -3,28 +3,43 @@ import FormTest from'./components/form'
 import NumberList from'./components/NumberList'
 import React,{useRef,useState} from 'react'
 function App() {
-  const numbers = [1, 2, 3, 4, 5]
-  //create refs for input fields 
-  const nameRef = useRef()
-  const emailRef = useRef()
-  const ageRef = useRef()
-  const [details,setDetails]= useState(null)
+ 
+  const titleImp = useRef()
+  const yearImp = useRef()
 
-  const handleSubmit = (e) =>{
-    e.preventDefault()
-    const name = nameRef.current.value;
-    const email = emailRef.current.value;
-    const age = ageRef.current.value;
-    setDetails({name,email,age})
+  const [data,setData]= useState(null)
 
-  }
+
+   const handleSubmit = (e) =>{
+    e.preventDefault();
+    const title = titleImp.current.value
+    const year = yearImp.current.value
+    setData({titleImp:title,yearImp:year})
+    titleImp.current.value = ""
+    yearImp.current.value = ""
+
+   
+   //console.log(titleInp,yearInp)
+    
+   }
+ 
 
   return (
   <div>
-
-   <FormTest title="my freind "/> 
-{   <NumberList items={numbers}/>
- }  
+   <form onSubmit={handleSubmit}>
+      <div><label>Title : </label>
+      <input type="text" name="title" required id="title" ref={titleImp}  />
+      </div>
+      <div>
+      <label>year : </label>
+      <input type="number" name="title" required id="title" ref={yearImp} />
+      </div>
+     <button type="submit">click me </button>
+    </form>
+  {!data?<h3>no data ...</h3>:<div>
+      {data.titleImp}<br />
+      {data.yearImp}
+    </div>}
  
  
  </div>  
